2010bcab58f2e22e817fa69fa5ac7997afc7c57f
Saran K <saransaran7222@gmail.com>
2026-05-29 12:58:50 +0530

    Update tic_tac_toe.py

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,75 +1,60 @@
+import streamlit as st
 import random
 
-# create empty board
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+# create board
+if "board" not in st.session_state:
+    st.session_state.board = [" "] * 9
+
+board = st.session_state.board
+
+st.title("Tic Tac Toe")
+st.write("You are X and Computer is O")
 
 # show board
-def show_board():
-    print()
-    print(board[0], "|", board[1], "|", board[2])
-    print("--+---+--")
-    print(board[3], "|", board[4], "|", board[5])
-    print("--+---+--")
-    print(board[6], "|", board[7], "|", board[8])
-    print()
+for i in range(0, 9, 3):
+    st.write(board[i] + " | " + board[i+1] + " | " + board[i+2])
 
-# check winner
-def winner(mark):
-    if (board[0] == mark and board[1] == mark and board[2] == mark) or \
-       (board[3] == mark and board[4] == mark and board[5] == mark) or \
-       (board[6] == mark and board[7] == mark and board[8] == mark) or \
-       (board[0] == mark and board[3] == mark and board[6] == mark) or \
-       (board[1] == mark and board[4] == mark and board[7] == mark) or \
-       (board[2] == mark and board[5] == mark and board[8] == mark) or \
-       (board[0] == mark and board[4] == mark and board[8] == mark) or \
-       (board[2] == mark and board[4] == mark and board[6] == mark):
-        return True
+# winner check
+def check(mark):
+    win = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ]
+
+    for x in win:
+        if board[x[0]] == mark and board[x[1]] == mark and board[x[2]] == mark:
+            return True
     return False
 
-# check draw
-def draw():
-    return " " not in board
+# user input
+pos = st.number_input("Enter position (1-9)", 1, 9)
 
-print("Welcome to Tic Tac Toe")
-print("You are X and Computer is O")
+if st.button("Play"):
 
-while True:
-    show_board()
-
-    # player move
-    pos = int(input("Enter position (1-9): ")) - 1
+    pos = pos - 1
 
     if board[pos] == " ":
         board[pos] = "X"
+
+        if check("X"):
+            st.success("You Won!")
+
+        else:
+            empty = []
+
+            for i in range(9):
+                if board[i] == " ":
+                    empty.append(i)
+
+            if empty:
+                comp = random.choice(empty)
+                board[comp] = "O"
+
+                if check("O"):
+                    st.error("Computer Won!")
+
     else:
-        print("Already filled. Try again.")
-        continue
+        st.warning("Position already filled")
 
-    if winner("X"):
-        show_board()
-        print("You won!")
-        break
-
-    if draw():
-        show_board()
-        print("Match Draw")
-        break
-
-    # computer move
-    empty = []
-    for i in range(9):
-        if board[i] == " ":
-            empty.append(i)
-
-    comp = random.choice(empty)
-    board[comp] = "O"
-
-    if winner("O"):
-        show_board()
-        print("Computer won!")
-        break
-
-    if draw():
-        show_board()
-        print("Match Draw")
-        break
+    st.rerun()
